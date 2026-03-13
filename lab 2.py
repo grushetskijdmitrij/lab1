1c28e520b881d6495f5f3f897f6af67915442e12
Dmitry Grushetsky <grushetskijdmitrij@gmail.com>
2026-03-13 22:36:45 +0300

    lab2 start

---
+++ lab 2.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LogisticRegression
+
+from sklearn.metrics import mean_absolute_error
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import r2_score
+
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
