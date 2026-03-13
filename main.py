833480aa12d5c10097f02091a3d9ac3b4a0ff5cd
Dmitry Grushetsky <grushetskijdmitrij@gmail.com>
2026-03-13 21:24:30 +0300

    missing values

--- main.py
+++ main.py
@@ -3,3 +3,10 @@ import pandas as pd
 df = pd.read_csv("fifa_ranking_2022-10-06.csv")
 
 print(df.head())
+
+print(df.info())
+
+print("\nКоличество пропущенных значений в каждом столбце:")
+
+missing_values = df.isnull().sum()
+print(missing_values)
